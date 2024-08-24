7b5ec3d80b9f430e1a01985c6293432aff307f92
Axel Choteau <axelc37@outlook.fr>
2024-08-24 21:23:07 +0200

    Mouse movement

--- Actors/Player/Movement/movement_processor.gd
+++ Actors/Player/Movement/movement_processor.gd
@@ -1,7 +1,16 @@
-﻿## This class is used to tuck away movement code from the Player class.
+## This class is used to tuck away movement code from the Player class.
+@icon("res://Assets/Class icons/running.svg")
 class_name MovementProcessor
 extends Node
 
 func process_movement(_character: CharacterBody3D, _delta: float, _move_max_speed: float, _move_jump_impulse: float) -> void:
-	print("MovementProcessor is considered an abstract class and should not be used directly.")
+	_warn_abstract_class()
 	pass
+
+func rotate_camera(_node_to_rotate: Node3D, _motion: Vector2, _sensitivity: float) -> void:
+	_warn_abstract_class()
+	pass
+
+
+func _warn_abstract_class():
+	print("MovementProcessor is considered an abstract class and should not be used directly.")
--- Actors/Player/player.gd
+++ Actors/Player/player.gd
@@ -1,12 +1,38 @@
 extends CharacterBody3D
 
+const MOUSE_MOTION_SCALE_DOWN_FACTOR: float = 1500
+
+@export var mouse_sensitivity: float = 0.25
+
+
 @export var move_max_speed: float = 5
 @export var move_jump_impulse: float = 10
 
 @export var movement_processor: MovementProcessor = null
 
+@onready var camera_pivot: Node3D = %CameraPivot
+
 func _physics_process(delta: float) -> void:
 	process_movement(delta)
+
+
+func _input(event: InputEvent):
+	if event.is_action_pressed("ui_cancel"):
+		print("Releasing mouse...")
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		print("Capturing mouse...")
+		if Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		var scaled_motion = event.screen_relative * (1.0 / MOUSE_MOTION_SCALE_DOWN_FACTOR)
+		rotate_y(-scaled_motion.x * mouse_sensitivity)
+		camera_pivot.rotate_x(-scaled_motion.y * mouse_sensitivity)
+
+		var camera_rot = camera_pivot.rotation_degrees
+		camera_rot.x = clamp(camera_rot.x, -90, 90)
+		camera_pivot.rotation_degrees = camera_rot
+
 
 
 func process_movement(delta: float) -> void:
