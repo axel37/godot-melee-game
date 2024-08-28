b600661255a795294abcf60dc153583ae2f65d57
Axel Choteau <axelc37@outlook.fr>
2024-08-28 22:06:21 +0200

    DamageDealer _set_enabled

--- Actors/Damage/damage_dealer.gd
+++ Actors/Damage/damage_dealer.gd
@@ -4,10 +4,15 @@ class_name DamageDealer
 extends Area3D
 
 const LOG_CODE_DAMAGE_DEALT = "DAMAGE-001"
+const LOG_CODE_SET_ENABLED = "DAMAGE-002"
+
 
 signal dealt_damage()
 
 @export var amount: float = 0
+
+@export var enabled: bool = true:
+	set = _set_enabled
 
 func _ready() -> void:
 	area_entered.connect(_on_area_entered)
@@ -17,3 +22,12 @@ func _on_area_entered(area: Area3D) -> void:
 		var damage_receiver: DamageReceiver = area as DamageReceiver
 		Global.log(LOG_CODE_DAMAGE_DEALT, "%s dealt damage to %s" % [name, damage_receiver.name])
 		dealt_damage.emit()
+
+func _set_enabled(value: bool):
+	if enabled == value:
+		return  # TODO : Avoid redundant sets (this is being called every frame !)
+
+	enabled = value
+	monitorable = value
+	monitoring = value
+	Global.log(LOG_CODE_SET_ENABLED, "%s : _set_enabled %s" % [name, value])
--- Autoload/global.gd
+++ Autoload/global.gd
@@ -3,4 +3,7 @@ extends Node
 @onready var _logger: Logger = %Logger
 
 func log(code: String, message: String) -> void:
+	if _logger == null:
+		push_warning("Tried to log, but _logger was null !")
+		return
 	_logger.log(code, message)
