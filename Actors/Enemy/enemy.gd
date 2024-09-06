6be58d6adac46a1c47395bcf4681aaf7add8206b
Axel Choteau <axelc37@outlook.fr>
2024-09-07 00:36:41 +0200

    Fixed animationplayer variable referance

--- Actors/Enemy/enemy.gd
+++ Actors/Enemy/enemy.gd
@@ -4,7 +4,8 @@ const LOG_CODE_DAMAGE_RECEIVED = "ENEMY-001"
 const LOG_CODE_DAMAGE_BLOCKED = "ENEMY-002"
 
 
-@onready var animation_player: AnimationPlayer = %AnimationPlayer
+@onready var weapon_animation_player: AnimationPlayer = %WeaponAnimationPlayer
+@onready var hurt_animation_player: AnimationPlayer = %HurtAnimationPlayer
 @onready var animation_tree: AnimationTree = %AnimationTree
 
 
@@ -13,11 +14,11 @@ func _process(delta: float) -> void:
 
 
 func _on_damage_receiving_handler_received_damage() -> void:
-	animation_player.play("got_hurt")
+	hurt_animation_player.play("got_hurt")
 	Global.log(LOG_CODE_DAMAGE_RECEIVED, "%s received damage." % [name])
 
 
 func _on_enemy_damage_receiving_handler_blocked_damage() -> void:
 	Global.log(LOG_CODE_DAMAGE_BLOCKED, "%s blocked damage." % [name])
 	animation_tree["parameters/conditions/blocked_damage"] = true
-	animation_player.play("blocked_damage")
+	weapon_animation_player.play("blocked_damage")
