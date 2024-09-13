597184fa8955ec664516e8b9c30332e8da7ee627
Axel Choteau <axelc37@outlook.fr>
2024-09-13 20:22:40 +0200

    Adjusted player guarded animation

--- Actors/Player/player.gd
+++ Actors/Player/player.gd
@@ -92,4 +92,5 @@ func _on_damage_dealing_handler_was_blocked() -> void:
 
 
 func _on_damage_receiving_handler_blocked_damage() -> void:
+	state_machine.travel("guarded")
 	Global.log(LOG_CODE_BLOCKED_INCOMING_DAMAGE, "% has guarded against an incoming attack.")
