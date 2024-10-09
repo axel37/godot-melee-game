1be603b5e9cbdf36faa4f9d6d09ccca8905fed6a
Axel Choteau <axelc37@outlook.fr>
2024-10-09 13:29:33 +0200

    Imported animations are fucking broken wtf is this shit

--- Actors/BlenderEnemy/blender_enemy.gd
+++ Actors/BlenderEnemy/blender_enemy.gd
@@ -10,6 +10,7 @@ const LOG_CODE_TOOK_DAMAGE = "BLENDER-ENEMY-01"
 
 func _on_damage_receiving_handler_received_damage() -> void:
 	Global.log(LOG_CODE_TOOK_DAMAGE, "%s took damage." % name)
+	state_machine.travel("hurt")
 
 
 func _on_target_detector_body_entered(body: Node3D) -> void:
