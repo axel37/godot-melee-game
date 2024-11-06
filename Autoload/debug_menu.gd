5edabaac8f66db664be2ee7a19c1936829e8043c
Axel <login+git@axelc.fr>
2024-11-06 12:22:24 +0100

    Sync values from settings on menu open

--- Autoload/debug_menu.gd
+++ Autoload/debug_menu.gd
@@ -3,9 +3,6 @@ extends Node
 
 var menu_is_open: bool = false
 
-## Will attempt to retrieve values from Global autoload until true
-var values_have_been_initialized = false
-
 # Values bound to the ImGui menus. Needed since we can't bind existing values directly...
 var debug_overlay_enable: Array = []
 var debug_overlay_enable_damage_receiver: Array = []
@@ -17,13 +14,15 @@ var debug_overlay_enable_damage_dealer: Array = []
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("open_debug_menu"):
 		menu_is_open = not menu_is_open
-		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE if menu_is_open else Input.MOUSE_MODE_CAPTURED
+		if menu_is_open:
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+			_get_values_from_settings()
+		else:
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 
 ## Display the menu !
 func _process(_delta: float) -> void:
-	_init_values()
-
 	if menu_is_open:
 		ImGui.Begin("Debug menu")
 		ImGui.SeparatorText("Debug overlays")
@@ -36,11 +35,11 @@ func _process(_delta: float) -> void:
 
 		ImGui.End()
 
-		_update_variables()
+		_push_values_to_settings()
 
 # Retrieve initial values once
-func _init_values():
-	if values_have_been_initialized or Global.game_settings == null: return
+func _get_values_from_settings():
+	if Global.game_settings == null: return
 
 	# Initialize values
 	debug_overlay_enable = [Global.game_settings.debug_overlay_enable]
@@ -48,12 +47,9 @@ func _init_values():
 	debug_overlay_enable_damage_blocker = [Global.game_settings.debug_overlay_enable_damage_blocker]
 	debug_overlay_enable_damage_dealer = [Global.game_settings.debug_overlay_enable_damage_dealer]
 
-	# Mark job as done, so it's only executed once
-	values_have_been_initialized = true
-
 ## Manually update the variables, since we can't bind them directly...
 # TODO : Find a way to do this automatically, I just forgot to add new variables to this method ! ARGH
-func _update_variables():
+func _push_values_to_settings():
 	Global.game_settings.debug_overlay_enable = debug_overlay_enable[0]
 	Global.game_settings.debug_overlay_enable_damage_blocker = debug_overlay_enable_damage_blocker[0]
 	Global.game_settings.debug_overlay_enable_damage_receiver = debug_overlay_enable_damage_receiver[0]
