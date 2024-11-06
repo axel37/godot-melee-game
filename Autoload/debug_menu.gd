effc2fad3b8e4d643899b929e9dbdaa148e5ae42
Axel <login+git@axelc.fr>
2024-11-06 13:24:25 +0100

    Time scale slider

--- Autoload/debug_menu.gd
+++ Autoload/debug_menu.gd
@@ -4,10 +4,12 @@ extends Node
 var menu_is_open: bool = false
 
 # Values bound to the ImGui menus. Needed since we can't bind existing values directly...
+var debug_time_scale: Array = []
 var debug_overlay_enable: Array = []
 var debug_overlay_enable_damage_receiver: Array = []
 var debug_overlay_enable_damage_blocker: Array = []
 var debug_overlay_enable_damage_dealer: Array = []
+
 
 
 ## Open / Close the menu and release / capture the mouse
@@ -25,6 +27,7 @@ func _input(event: InputEvent) -> void:
 func _process(_delta: float) -> void:
 	if menu_is_open:
 		ImGui.Begin("Debug menu")
+		ImGui.SliderFloat("Time scale", debug_time_scale, 0.1, 3)
 		ImGui.SeparatorText("Debug overlays")
 		ImGui.Checkbox("Enable", debug_overlay_enable)
 		ImGui.BeginDisabled(!debug_overlay_enable[0])
@@ -42,6 +45,7 @@ func _get_values_from_settings():
 	if Global.game_settings == null: return
 
 	# Initialize values
+	debug_time_scale = [Global.time_scale]
 	debug_overlay_enable = [Global.game_settings.debug_overlay_enable]
 	debug_overlay_enable_damage_receiver = [Global.game_settings.debug_overlay_enable_damage_receiver]
 	debug_overlay_enable_damage_blocker = [Global.game_settings.debug_overlay_enable_damage_blocker]
@@ -50,6 +54,7 @@ func _get_values_from_settings():
 ## Manually update the variables, since we can't bind them directly...
 # TODO : Find a way to do this automatically, I just forgot to add new variables to this method ! ARGH
 func _push_values_to_settings():
+	Global.time_scale = debug_time_scale[0]
 	Global.game_settings.debug_overlay_enable = debug_overlay_enable[0]
 	Global.game_settings.debug_overlay_enable_damage_blocker = debug_overlay_enable_damage_blocker[0]
 	Global.game_settings.debug_overlay_enable_damage_receiver = debug_overlay_enable_damage_receiver[0]
