c110290e4e5fb7fd77f725da4053308a7cdbc617
Axel Choteau <axelc37@outlook.fr>
2024-11-29 17:18:02 +0100

    Healthbar : adjusted default values

--- Interface/health_bar.gd
+++ Interface/health_bar.gd
@@ -11,10 +11,12 @@ extends Control
 @export var max_value: float = 100
 @export_range(0, 100, .1) var current_value: float = 100:
 	set = _set_value
-@export_range(0, 3, .1) var buffer_time: float = .7
+@export_range(.1, 3, .1) var buffer_delay: float = .4
+@export_range(0, 1.5, .1) var buffer_time: float = .3
 
 # TODO : Allow configuring colors
-# TODO : Tween buffer
+# TODO : Immediately update buffer when health goes UP
+# TODO : Healing buffer ?
 
 func _ready() -> void:
 	buffer_timer.timeout.connect(_on_buffer_timeout)
@@ -24,11 +26,11 @@ func _set_value(value: float) -> void:
 
 	viz_current.size.x = _get_viz_size_for_value(current_value)
 
-	buffer_timer.start(buffer_time)
+	buffer_timer.start(buffer_delay)
 
 func _on_buffer_timeout() -> void:
 	var next_value:float = _get_viz_size_for_value(current_value)
-	create_tween().tween_property(viz_prev, "size:x", next_value, 1)
+	create_tween().set_ease(Tween.EASE_IN).tween_property(viz_prev, "size:x", next_value, buffer_time)
 
 func _get_viz_size_for_value(next_size: float) -> float:
 	return viz_bg.size.x * (next_size / max_value)
