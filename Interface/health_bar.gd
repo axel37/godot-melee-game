8d3afafb272e0ef6bf43e670b9fbf49c34f17c37
Axel Choteau <axelc37@outlook.fr>
2024-11-29 17:10:51 +0100

    Healthbar : tweening

--- Interface/health_bar.gd
+++ Interface/health_bar.gd
@@ -14,6 +14,7 @@ extends Control
 @export_range(0, 3, .1) var buffer_time: float = .7
 
 # TODO : Allow configuring colors
+# TODO : Tween buffer
 
 func _ready() -> void:
 	buffer_timer.timeout.connect(_on_buffer_timeout)
@@ -21,10 +22,13 @@ func _ready() -> void:
 func _set_value(value: float) -> void:
 	current_value = maxf(min_value, value)
 
-	viz_current.size.x = viz_bg.size.x * (current_value / max_value)
+	viz_current.size.x = _get_viz_size_for_value(current_value)
 
 	buffer_timer.start(buffer_time)
 
 func _on_buffer_timeout() -> void:
-	# TODO : Duplicate code with set_value, extract ?
-	viz_prev.size.x = viz_bg.size.x * (current_value / max_value)
+	var next_value:float = _get_viz_size_for_value(current_value)
+	create_tween().tween_property(viz_prev, "size:x", next_value, 1)
+
+func _get_viz_size_for_value(next_size: float) -> float:
+	return viz_bg.size.x * (next_size / max_value)
